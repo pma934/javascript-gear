b628c5cef48549855e11b613c575d8bf12b890ff
Zhou Xuanzhuo <358409584@qq.com>
2018-10-12 15:41:58 +0800

    添加一些js

---
+++ 高阶函数.js
@@ -0,0 +1,23 @@
+var add = function (a, b) {
+    if (b == undefined)
+        b = 0;
+    return a + b;
+}
+
+var chen = function (a, b) {
+    if (b == undefined)
+        b = 1;
+    return a * b;
+}
+
+var Alljs = function (array, f) {
+    var p;
+    for(var k=0;k<array.length;k++){
+        p=f(array[k],p);
+    }
+    console.log(p);
+    return p;
+}
+
+Alljs([2,3,4],add);
+Alljs([2,3,4],chen);
