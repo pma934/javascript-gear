88cd3e599810a69f770e11e5ea8e28db708db13b
Zhou Xuanzhuo <358409584@qq.com>
2019-05-23 15:06:25 +0800

    ~

---
+++ 前端题/数字每3位加逗号.js
@@ -0,0 +1,17 @@
+// 写一个js函数，实现对一个数字每3位加一个逗号，如输入100000，输出100,000(不考虑负数，小数)
+
+function main(num) {
+
+    let res = num.toString()
+    let left = res.slice(0, res.length % 3)
+    if (left.length) {
+        left = left + ','
+    }
+    let right = res.slice(res.length % 3)
+    right = right.replace(/(\d{3})/g, $1 => $1 + ',')
+
+
+    return (left + right).slice(0, -1)
+}
+
+var str = main(234567)
