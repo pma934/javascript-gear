592b86fd99c74b6c82ff9ea8d90cb3ad223743c5
Zhou Xuanzhuo <358409584@qq.com>
2019-07-18 21:35:47 +0800

    ~

---
+++ 前端题/自写promise 简.js
@@ -0,0 +1,59 @@
+const WAIT = 'wait'
+const RSV = 'resolved'
+const RJS = 'rejected'
+
+function MyPromise(fn) {
+    const that = this
+    that.status = WAIT
+    that.value = null
+    that.rsvCb = []
+    that.rjsCb = []
+
+    function resolve(value) {
+        if (that.status === WAIT) {
+            that.status = RSV
+            that.value = value
+            that.rsvCb.map(cb=>cb(value))
+        }
+    }
+
+    function reject(value) {
+        if (that.status === WAIT) {
+            that.status = RJS
+            that.value = value
+            that.rjsCb.map(cb=>cb(value))
+        }
+    }
+
+    try {
+        fn(resolve, reject)
+    } catch (e) {
+        reject(e)
+    }
+}
+
+MyPromise.prototype.then = function (rsvFn, rjsFn) {
+    rsvFn = rsvFn instanceof Function ? rsvFn : () => rsvFn
+    rjsFn = rjsFn instanceof Function ? rjsFn : () => rjsFn
+
+    if (this.status === WAIT) {
+        this.rsvCb.push(rsvFn)
+        this.rjsCb.push(rjsFn)
+    }
+    if (this.status === RSV) {
+        rsvFn(this.value)
+    }
+    if (this.status === RJS) {
+        rjsFn(this.value)
+    }
+
+    
+}
+
+new MyPromise((resolve, reject) => {
+    console.log(123)
+    setTimeout(function () {
+        resolve(456)
+        reject(789)
+    }, 3000)
+}).then(res => console.log(res), err => console.log(err))
