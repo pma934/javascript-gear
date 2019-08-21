9c5e528b144c894e280541d31454618f0de1eb66
Zhou Xuanzhuo <358409584@qq.com>
2019-08-21 18:56:43 +0800

    我的完全版promise(可能仍有bug)

--- 前端题/手写Promise.all.js
+++ 前端题/手写Promise.all.js
@@ -1,22 +1,18 @@
 //传入的参数可能是promise，也可能就只是个参数
-function PromiseAll(promises) {
+Promise.all = function (promises) {
+    let resolveList = []
     return new Promise((resolve, reject) => {
-        let arr = []
-        if (promises.length === 0) {
-            resolve(arr)
-        } else {
-            for (let i = 0; i < promises.length; i++) {
-                Promise.resolve(promises[i]).then(res => {  //因为传入的可能就只是个参数，因此用resolve包装
-                // promises[i].then(res => {
-                    arr.push(res)
-                    if(i === promises.length - 1){
-                        resolve(arr)
-                    }
-                }, err => {
-                    reject(err)
-                })
-            }
-        }
+        promises.forEach(p => {
+            Promise.resolve(p).then(re => {
+                resolveList.push(re)
+                if (promises.length === resolveList.length) {
+                    //因为promise异步的原因，还是得放里面
+                    resolve(resolveList)
+                }
+            }, rj => {
+                reject(rj)
+            })
+        })
     })
 }
 
@@ -33,4 +29,4 @@ var d = new Promise((resolve,reject)=>{
     resolve('d')
 })
 
-PromiseAll([2,a,b,c,d]).then(res=>console.log(res))
+Promise.all([2,a,b,c,d]).then(res=>console.log(res),err=>console.log(err))
